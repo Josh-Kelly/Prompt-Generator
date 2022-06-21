738c08eb96ca8244788789393fafcabab59f0e74
Josh <joshuakelly8@gmail.com>
2022-06-21 17:34:19 -0600

    rename mainPage files to "index" files

---
+++ index.js
@@ -0,0 +1,20 @@
+function createMainPage() {
+    /* BUTTON DIVISION */
+    const buttonBookOne = document.createElement('button');
+    buttonBookOne.id = 'buttonBookOne';
+    buttonBookOne.innerHTML = 'Go to book one';
+    buttonBookOne.setAttribute("onclick", "createBookOneMain()");
+    document.body.appendChild(buttonBookOne);
+
+    const buttonBookTwo = document.createElement('button');
+    buttonBookTwo.id = 'buttonBookTwo';
+    buttonBookTwo.innerHTML = 'Go to book two';
+    buttonBookTwo.setAttribute("onclick", "createBookTwoMain()");
+    document.body.appendChild(buttonBookTwo);
+
+    const buttonBookThree = document.createElement('button');
+    buttonBookThree.id = 'buttonBookThree';
+    buttonBookThree.innerHTML = 'Go to book three';
+    buttonBookThree.setAttribute("onclick", "createBookThreeMain()");
+    document.body.appendChild(buttonBookThree);
+}
